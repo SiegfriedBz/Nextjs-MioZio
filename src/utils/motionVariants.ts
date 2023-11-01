5bd302ac1862500766f788c7492dd52ea0151cae
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-01 06:46:09 +0100

    add motionVariants

---
+++ src/utils/motionVariants.ts
@@ -0,0 +1,14 @@
+export const buttonEnterVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    scale: [1, 1.25, 1],
+    transition: {
+      delay: 1,
+      duration: 0.75,
+      ease: 'easeInOut',
+    },
+  },
+}
