742c795cec750d5f6ae2e8bda2d7e940e9824b4e
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-01 06:44:25 +0100

    update tw cfg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,44 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // // stone-100
+        // light: '#f7fafc',
+        // stone-50
+        light: '#f9fafb',
+        // slate-900
+        dark: '#1a202c',
+        primary: {
+          // red-500
+          DEFAULT: '#f56565',
+          // red-600
+          dark: '#e53e3e',
+          // red-400
+          light: '#fc8181',
+        },
+        secondary: {
+          // green-500
+          DEFAULT: '#48bb78',
+          // green-600
+          dark: '#38a169',
+          // green-400
+          light: '#68d391',
+        },
+        tertiary: {
+          //  amber-400
+          DEFAULT: '#fbbf24',
+          // amber-500
+          dark: '#f59e0b',
+          // amber-300
+          light: '#fcd34d',
+        },
+        quaternary: {
+          // rose-50
+          DEFAULT: '#fff1f2',
+        },
       },
     },
   },
