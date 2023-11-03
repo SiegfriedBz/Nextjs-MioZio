c8ddfe757ee4c9d500cf520fdfebad2dbfc6d915
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-03 17:17:07 +0100

    add prisma adapter + init schema + migrations

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,7 +1,10 @@
 import { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from './prismaClient'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
