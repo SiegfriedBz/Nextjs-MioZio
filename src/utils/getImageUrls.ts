0b38f9e4227cad090b99c0d76709808fce4cd371
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-04 16:53:42 +0100

    refactor getImages functions

--- src/utils/getImageUrls.ts
+++ src/utils/getImageUrls.ts
@@ -1,22 +1,27 @@
-type SocialImageType = {
-  title: string
-  cloudName: string
-  imagePublicID: string
-  cloudinaryUrlBase?: string
-  version?: string | null
-  titleFont?: string
-  titleExtraConfig?: string
-  imageWidth?: number
-  imageHeight?: number
-  textAreaWidth?: number
-  textAreaHeight?: number
-  textLeftOffset?: number
-  textBottomOffset?: number
-  textColor?: string
-  titleFontSize?: number
+export function getImageUrl(imageId: string) {
+  try {
+    return `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}${imageId}`
+  } catch (error) {
+    console.log(error)
+    return ''
+  }
 }
 
-export default function generateSocialImage({
+export async function getBase64ImageUrl(imageId: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}w_100/e_blur:300,q_auto,f_webp/${imageId}`
+    )
+    const buffer = await response.arrayBuffer()
+    const data = Buffer.from(buffer).toString('base64')
+    return `data:image/webp;base64,${data}`
+  } catch (error) {
+    console.log(error)
+    return ''
+  }
+}
+
+export function getSocialImage({
   title,
   cloudName,
   imagePublicID,
