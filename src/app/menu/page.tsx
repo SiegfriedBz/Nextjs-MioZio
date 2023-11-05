81c74a78bf8b36332f34a7c7d86fe804f539ef47
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-05 07:53:49 +0100

    update getData Menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,57 +1,71 @@
-import type { MenuCategoryDataType } from '@/data'
-import { menuCategoryData } from '@/data'
-import { getBase64ImageUrl, getImageUrl } from '@/utils/cloudinaryUtils'
+import { getBase64ImageUrl, getImageUrl } from '@/utils/getImageUrls'
 import Link from 'next/link'
 import { twMerge } from 'tailwind-merge'
+import type { MenuCategoryType } from '@/utils/types'
+import { notFound } from 'next/navigation'
 
 // trick tailwind
 const possibleBgColors = ['bg-[#009345]', 'bg-[#CF2B36]', 'bg-transparent']
 
-type FullImageType = MenuCategoryDataType & {
-  blurDataUrl: string
-}
+const handleCache =
+  process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store'
 
 async function getData() {
-  const menuCategoryDataPromises = menuCategoryData.map(async (data) => {
-    const src = getImageUrl(data.src)
-    const blurDataUrl = await getBase64ImageUrl(data.src)
-    const fullData: FullImageType = {
-      ...data,
-      src,
-      blurDataUrl,
-    }
+  try {
+    const response = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/menuCategories`,
+      {
+        cache: handleCache,
+      }
+    )
+    if (!response.ok) throw new Error('Network response was not ok.')
 
-    return fullData
-  })
+    const { menuCategories } = await response.json()
 
-  const menuCategories = await Promise.all(menuCategoryDataPromises)
+    const promises = menuCategories.map(async (data: MenuCategoryType) => {
+      const img = getImageUrl(data.img!)
+      const imgBlur = await getBase64ImageUrl(data.img!)
+      const fullData: MenuCategoryType = {
+        ...data,
+        img,
+        imgBlur,
+      }
 
-  return { menuCategories }
+      return fullData
+    })
+
+    const menuCategoriesData: MenuCategoryType[] = await Promise.all(promises)
+
+    return menuCategoriesData
+  } catch (error) {
+    console.log(`Error: ${error}`)
+    return notFound()
+  }
 }
 
 const Menu = async () => {
-  const { menuCategories } = await getData()
+  const menuCategoriesData = await getData()
 
   return (
     <div className='md:h-section flex flex-col px-4 md:flex-row md:py-24 lg:px-16 xl:px-32 2xl:px-48 [&>*:nth-child(even)]:border-b-2 [&>*:nth-child(even)]:border-t-2 [&>*:nth-child(even)]:border-light md:[&>*:nth-child(even)]:border-0'>
-      {menuCategories?.map((menuCategory) => {
+      {menuCategoriesData?.map((menuCategory) => {
         // CATEGORY
         return (
           <Link
             href={`/menu/${menuCategory.slug}`}
             className={`${menuCategory?.bgColor} h-section-third flex w-full bg-cover bg-center hover:shadow-md md:h-full md:w-[33vw]`}
             key={menuCategory.id}
-            style={{ backgroundImage: `url(${menuCategory.src})` }}
+            style={{ backgroundImage: `url(${menuCategory.img})` }}
           >
             {/* category text container */}
             <div
               className={`flex w-1/2 flex-col items-start justify-start space-y-1 py-4 ps-4 sm:space-y-4 sm:p-8 xl:justify-center 2xl:space-y-8 ${menuCategory?.textColor}`}
             >
               <h3 className='text-2xl font-bold uppercase tracking-wide opacity-80 sm:text-3xl md:text-5xl'>
-                {menuCategory.title}
+                {menuCategory.name}
               </h3>
               <h4 className='text-base opacity-60 sm:text-lg md:text-xl lg:text-2xl'>
-                {menuCategory.desc}
+                {menuCategory.description}
               </h4>
               <button
                 className={twMerge(
