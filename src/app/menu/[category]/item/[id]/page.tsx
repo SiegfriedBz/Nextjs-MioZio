c3f5163137e6f5d4dcfb7d0baf50dc74c61d4b2f
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-05 07:54:28 +0100

    update getData Item page

--- src/app/menu/[category]/item/[id]/page.tsx
+++ src/app/menu/[category]/item/[id]/page.tsx
@@ -1,35 +1,45 @@
-import { menuItemByCategoryData } from '@/data'
 import { notFound } from 'next/navigation'
-import type { ItemType } from '@/data'
-import { FullItemType } from '@/app/page'
-import { getBase64ImageUrl, getImageUrl } from '@/utils/cloudinaryUtils'
+import { getBase64ImageUrl, getImageUrl } from '@/utils/getImageUrls'
 import Image from 'next/image'
 import ItemMiniCart from '@/app/components/menu/category/item/ItemMiniCart'
+import type { MenuItemType } from '@/utils/types'
 
-async function getData({ category, id }: { category: string; id: string }) {
-  const itemData = menuItemByCategoryData[category].find(
-    (item) => item.id === +id
-  )
+const handleCache =
+  process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store'
 
-  if (!itemData) {
-    return notFound()
-  }
+async function getData(id: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/menuItems?id=${id}`,
+      {
+        cache: handleCache,
+      }
+    )
+    if (!response.ok) throw new Error('Network response was not ok.')
 
-  const itemDataPromise = async (data: ItemType) => {
-    const src = getImageUrl(data.src)
-    const blurDataUrl = await getBase64ImageUrl(data.src)
-    const fullData: FullItemType = {
-      ...itemData,
-      src,
-      blurDataUrl,
+    const { menuItems } = await response.json()
+    const menuItem = menuItems?.[0] as MenuItemType
+
+    if (!menuItem) throw new Error('MenuItem not found.')
+
+    const menuItemPromise = async (menuItem: MenuItemType) => {
+      const img = getImageUrl(menuItem.img!)
+      const imgBlur = await getBase64ImageUrl(menuItem.img!) // Wait for the async result
+      const fullData: MenuItemType = {
+        ...menuItem,
+        img,
+        imgBlur,
+      }
+
+      return fullData
     }
 
-    return fullData
+    const menuItemData = menuItemPromise(menuItem)
+    return menuItemData
+  } catch (error) {
+    console.log(`Error: ${error}`)
+    return notFound()
   }
-
-  const item = await itemDataPromise(itemData)
-
-  return item
 }
 
 type ItemProps = {
@@ -40,29 +50,32 @@ type ItemProps = {
 }
 
 const Item = async ({ params }: ItemProps) => {
-  const { category, id } = params
+  const { id: menuItemId } = params
 
-  const item = await getData({ category, id })
+  const menuItemData = await getData(menuItemId)
 
   return (
     <section className='flex flex-col items-center justify-around space-y-1 p-4 sm:space-y-2 sm:px-16 md:flex-row md:space-x-4 md:px-8 lg:px-24 xl:px-32 2xl:px-48'>
       {/* IMAGE CONTAINER */}
       <div className='relative h-full w-full flex-1 pt-1'>
         <Image
-          src={item.src}
-          alt={item.alt}
+          src={menuItemData.img}
+          placeholder='blur'
+          blurDataURL={menuItemData.imgBlur}
+          alt={menuItemData.name}
           fill={true}
-          className='object-contain transition-all duration-300 md:hover:rotate-[8deg] md:hover:scale-105'
+          sizes='(max-width: 768px) 50vw,(max-width: 1024px) 33vw, 50vw'
+          className='rounded-3xl object-contain transition-all duration-300 md:hover:rotate-[8deg] md:hover:scale-105'
         />
       </div>
 
       {/* TEXT CONTAINER */}
       <div className='flex flex-1 flex-col space-y-1 px-2 text-primary sm:space-y-2 md:space-y-8'>
         <h2 className='text-2xl font-bold uppercase tracking-wide md:text-3xl'>
-          {item.title}
+          {menuItemData.name}
         </h2>
-        <h3 className='text-sm md:text-base'>{item.desc}</h3>
-        <ItemMiniCart item={item} />
+        <h3 className='text-sm md:text-base'>{menuItemData.description}</h3>
+        <ItemMiniCart item={menuItemData} />
       </div>
     </section>
   )
