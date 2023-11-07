b68d3e398f49523c85fd960f0d8b6f318493d0e2
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-07 22:15:37 +0100

    handleCache note

--- src/utils/handleCache.ts
+++ src/utils/handleCache.ts
@@ -1,2 +1,2 @@
-export const handleCache = 'no-store'
-// process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store'
+export const handleCache = 'no-store' // similar to getServerSideProps
+// 'force-cache', default, similar to getStaticProps
