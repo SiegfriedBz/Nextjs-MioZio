e4c2fac447f13d65e6dfc397f3ec64faf88672ab
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-08 00:41:22 +0100

    refactor Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { AuthProviders } from './context/AuthProviders'
-import { AppContextProvider } from './context/appContext'
-import QueryProvider from './context/QueryProvider'
+import Providers from './context/Providers'
 
 const roboto = Roboto({
   weight: ['100', '300', '400', '700'],
@@ -27,16 +25,12 @@ export default function RootLayout({
   return (
     <html lang='en' className='scroll-smooth'>
       <body className={`text-dark dark:text-light ${roboto.className}`}>
-        <AuthProviders>
-          <QueryProvider>
-            <AppContextProvider>
-              <Header />
-              {/* fixed header with h-24 */}
-              <main className='mt-24'>{children}</main>
-              <Footer />
-            </AppContextProvider>
-          </QueryProvider>
-        </AuthProviders>
+        <Providers>
+          <Header />
+          {/* fixed header with h-24 */}
+          <main className='mt-24'>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
