95bfe29c881cfa02dba9ed98c22fb7dcfee3e96b
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-12 13:10:16 +0100

    fix OrderStatusEnum + MenuCategorySlugEnum types

--- src/types.ts
+++ src/types.ts
@@ -17,9 +17,6 @@ export type MenuCategoryType = {
   bgColor?: string
   textColor?: string
   menuItems?: MenuItemType[]
-
-  createdAt?: Date
-  updatedAt?: Date
 }
 
 export enum MenuCategorySlugEnum {
@@ -38,33 +35,28 @@ export type MenuItemType = {
   isFeatured?: boolean
   options?: MenuOptionType[]
 
-  categorySlug: MenuCategoryType['slug'] // FK to MenuCategory model
-
-  createdAt?: Date
-  updatedAt?: Date
+  categorySlug: MenuCategorySlugEnum // FK to MenuCategory model
 }
 
 export type MenuOptionType = { name: string; additionalPrice: number }
 
 export type OrderType = {
   id?: string
-  status?: OrderStatusEnum
+  status: OrderStatusEnum
   totalPrice: number
   intent_id?: string // Stripe PaymentIntent
   cartItems: CartItemType[]
-
-  userEmail: UserType['email'] // FK to User model
-
   createdAt?: Date
-  updatedAt?: Date
+
+  userEmail: string // FK to User model
 }
 
 export enum OrderStatusEnum {
-  PENDING = 'PENDING',
-  PAID = 'PAID',
-  ON_THE_WAY = 'ON_THE_WAY',
-  DELIVERED = 'DELIVERED',
-  CANCELLED = 'CANCELLED',
+  PENDING = 'pending',
+  PAID = 'paid',
+  ON_THE_WAY = 'on the way',
+  DELIVERED = 'delivered',
+  CANCELLED = 'cancelled',
 }
 
 export type CartItemType = {
@@ -73,7 +65,7 @@ export type CartItemType = {
   name: string
   totalPrice: number
   quantity: number
-  selectedOptionName: MenuOptionType['name']
+  selectedOptionName: string
   img?: string
 
   orderId?: string // FK to Order model
@@ -88,23 +80,4 @@ export type PageImageType = {
   img: string
   imgBlur?: string
   alt: string
-}
-
-//
-export type SocialImageType = {
-  title: string
-  cloudName: string
-  imagePublicID: string
-  cloudinaryUrlBase?: string
-  version?: string | null
-  titleFont?: string
-  titleExtraConfig?: string
-  imageWidth?: number
-  imageHeight?: number
-  textAreaWidth?: number
-  textAreaHeight?: number
-  textLeftOffset?: number
-  textBottomOffset?: number
-  textColor?: string
-  titleFontSize?: number
 }
